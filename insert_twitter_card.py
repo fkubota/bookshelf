9e844c7f069bd2374bc8a44decae0b30e9270d67
fkubota <rkkubotay@gmail.com>
2021-06-21 17:18:49 +0900

    [update]

--- insert_twitter_card.py
+++ insert_twitter_card.py
@@ -1,6 +1,6 @@
 # index.htmlに追加
 sq = "'"
-s = f'<head><meta property="og:description" content="最近やったことなどが時系列で簡単にまとめられています。" /><meta property="og:image" content="https://github.com/fkubota/timeline/blob/main/src/images/design.png?raw=true" /><meta property="twitter:site" content="@fkubota_" /><meta property="twitter:card" content="summary_large_image" /><meta property="twitter:title" content="fkubota{sq}s timeline"/>'
+s = f'<head><meta property="og:description" content="最近やったことなどが時系列で簡単にまとめられています。" /><meta property="og:image" content="https://github.com/fkubota/bookshelf/blob/main/src/images/design.png?raw=true" /><meta property="twitter:site" content="@fkubota_" /><meta property="twitter:card" content="summary_large_image" /><meta property="twitter:title" content="fkubota{sq}s timeline"/>'
 
 
 path = './docs/index.html'
