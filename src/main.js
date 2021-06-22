b3b0a81a81ed711111206e525c7e825c16da6829
fkubota <rkkubotay@gmail.com>
2021-06-22 13:42:33 +0900

    [update] font awesome

--- src/main.js
+++ src/main.js
@@ -4,6 +4,14 @@ import vuetify from './plugins/vuetify'
 import VueExpandableImage from 'vue-expandable-image'
 Vue.use(VueExpandableImage)
 
+// font-awesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+// import { faCoffee, faCheckCircle } from '@fortawesome/free-solid-svg-icons'
+import { faSpeakerDeck, faKaggle } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(faSpeakerDeck, faKaggle)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
