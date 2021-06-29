b2042aa5be0a6076b2c5c4ad7048f46d2c262464
fkubota <rkkubotay@gmail.com>
2021-06-30 07:05:36 +0900

    [update] pdf_extract_images.py, tableを抽出する機能の追加

--- pdf_extract_images.py
+++ pdf_extract_images.py
@@ -2,12 +2,15 @@ from ipdb import set_trace as st
 import os
 import sys
 import fitz
+import tabula
+import matplotlib.pyplot as plt
 
 
 def main(fname):
-    # st()
     save_dir = os.path.dirname(fname)
     num = 0
+
+    # image
     with fitz.open(fname) as doc:
         for i, page in enumerate(doc):
             for j, img in enumerate(page.getImageList()):
@@ -16,6 +19,21 @@ def main(fname):
                 save_path = f"{save_dir}/image{str(num).zfill(3)}.{x['ext']}"
                 with open(save_path, "wb") as ofh:
                     ofh.write(x['image'])
+
+
+    # table
+    dfs = tabula.read_pdf(fname, lattice=True, pages='all')
+    for df in dfs:
+        # graphとか拾ってくる時があるので、nullが0の時のみ取得
+        if (len(df)!=0) and (df.isnull().sum().sum()==0):
+            num += 1
+            save_path = f"{save_dir}/image{str(num).zfill(3)}.{x['ext']}"
+
+            fig, ax =plt.subplots(1, 1)
+            ax.axis('tight')
+            ax.axis('off')
+            ax.table(cellText=df.values, colLabels=df.columns, loc="center")
+            plt.savefig(save_path)
 
 
 if __name__ == "__main__":
